1787e3daf70a2aff7e4535997dbab658c3622ef5
Andy Hong <chili.tomat0@icloud.com>
2025-08-15 21:08:50 +0900

    fix: double 포맷으로 변경

--- src/main/java/mission/view/OutputView.java
+++ src/main/java/mission/view/OutputView.java
@@ -5,7 +5,7 @@ import mission.service.TimeService;
 
 public class OutputView {
     public static void outputMission(Date eta) {
-        System.out.printf("이동 시간은 %f시간 %f분으로 예측됩니다.",
+        System.out.printf("이동 시간은 %d시간 %d분으로 예측됩니다.",
                 TimeService.getHours(eta),
                 TimeService.getMinutes(eta)
         );
