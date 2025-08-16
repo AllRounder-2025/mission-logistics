98cc9c1cf8c86d0d7d21b2587054bceaddb0aef5
Andy Hong <chili.tomat0@icloud.com>
2025-08-16 16:09:21 +0900

    feat: 입력 데이터를 처리하는 View 모델

--- src/main/java/mission/view/InputView.java
+++ src/main/java/mission/view/InputView.java
@@ -1,4 +1,17 @@
 package mission.view;
 
+import mission.constants.Text;
+
 public class InputView {
+    public static String inputMission() {
+        System.out.println(Text.ACQUIRE_FOR_INPUT.getMessage());
+        System.out.println("> ");
+        String input;
+        try {
+            input = System.console().readLine();
+        } catch (Exception e) {
+            input = "";
+        }
+        return input;
+    }
 }
