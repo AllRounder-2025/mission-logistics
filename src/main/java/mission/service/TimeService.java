e6a1d598fa4c34e8e968127fac4a03752d0fae98
Andy Hong <chili.tomat0@icloud.com>
2025-08-16 17:22:15 +0900

    feat: 소요 시간 계산

--- src/main/java/mission/service/TimeService.java
+++ src/main/java/mission/service/TimeService.java
@@ -1,17 +1,9 @@
 package mission.service;
 
-import java.util.Date;
-
 public class TimeService {
-    public static long calculateDifference(Date date1, Date date2) {
-        return date1.getTime() - date2.getTime();
-    }
+    private static final double DEFAULT_SPEED_KMH = 60.0;
 
-    public static long getHours(Date date) {
-        return date.getTime() / (1000 * 60 * 60);
-    }
-
-    public static long getMinutes(Date date) {
-        return date.getTime() / (1000 * 60);
+    public static double predictTravelTimeHours(double distanceKm) {
+        return distanceKm / DEFAULT_SPEED_KMH;
     }
 }
